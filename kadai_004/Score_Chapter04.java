54dce75d51b6bb9353cc51a7645f4c44f139abe4
tomotaka1105 <105799958+tomotaka1105@users.noreply.github.com>
2023-11-03 14:04:11 +0900

    Add files via upload

---
+++ kadai_004/Score_Chapter04.java
@@ -0,0 +1,24 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+		String[] name = {"A", "B", "C", "D", "E", "D", "F", "G", "H", "I", "J"};
+		int[] score = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+		int sum = 0;
+		double ave = 0.0;
+
+		for( int i = 0; i < name.length - 1; i++ ) {
+			System.out.println( name[i] + "さんは" + score[i] + "点");
+		}
+
+		for (int n = 0; n < score.length - 1; n++) {
+			sum = sum + score[n];
+		}
+
+		ave = sum / score.length;
+		System.out.println("平均点は" + ave + "点です。");
+
+	}
+
+}
